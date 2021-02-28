a9b6c88afee50d1714900ad2c0237391e52ed598
Nathan McCaw <nathanmccaw@Analias-Mac-mini.local>
2021-02-28 18:54:21 -0500

    added inquirer options

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/Employee");
 
+// Prompt for user to enter Employee information
+
 function runInquirer() {
   const inquirerPromps = [
     {
       type: "input",
+      message: "Please enter your name",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter your employee ID",
+      name: "id",
+    },
+    {
+      type: "list",
+      message: "Please select your title",
+      choices: ["Manager", "Engineer", "Intern"],
+      name: "title",
     },
   ];
+  return inquirer.prompt(inquirerPromps);
 }
+
+// Function to take inquirer input a generator content
