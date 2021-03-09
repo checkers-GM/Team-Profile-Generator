12192e764d31af1392e42fa6d10495011bc1ee02
Nathan McCaw <nathanmccaw@Analias-Mac-mini.local>
2021-03-09 15:26:46 -0500

    finished basic styling for HTML output

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function runInquirerIntern() {
 async function runApplication() {
   // Loop through employee array to create each object
   let employeeArray = [];
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 1; i++) {
     const promise = new Promise((resolve, reject) => {
       runInquirerEmployee()
         .then(function ({ name, id, email, title }) {
@@ -78,6 +78,7 @@ async function runApplication() {
               this.employee = new Manager(name, id, email, officeID);
               employeeArray.push(employee);
               resolve("done");
+              console.log(employeeArray);
             });
           } else if (title === "Engineer") {
             runInquirerEngineer().then(function ({ github }) {
@@ -93,13 +94,85 @@ async function runApplication() {
             });
           }
         })
-        .catch(function (error) {
-          console.log(error);
+        .catch(function (err) {
+          console.log(err);
         });
     });
     const result = await promise;
     console.log(result);
   }
+
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="team col-12 justify-content-center">
+            <div class="card employee-card">
+              <div class="card-header">
+                <h2 class="card-title">Bob</h2>
+                <h3 class="card-title">Manager</h3>
+              </div>
+              <div class="card-body">
+                <ul class="list-items">
+                  <li class="list-group-item">ID: 1</li>
+                  <li class="list-group-item">
+                    Email: <a href="mailto:bob@office.com">bob@office.com</a>
+                  </li>
+                  <li class="list-group-item">Office Number: 5</li>
+                </ul>
+              </div>
+            </div>
+            <div class="card employee-card">
+              <div class="card-header">
+                <h2 class="card-title"></h2>
+                <h2 class="card-title">Sam</h2>
+                <h3 class="card-title">Engineer</h3>
+              </div>
+              <div class="card-body">
+                <ul class="list-items">
+                  <li class="list-group-item">ID: 1</li>
+                  <li class="list-group-item">
+                    Email: <a href="mailto:bob@office.com">Sam@office.com</a>
+                  </li>
+                  <li class="list-group-item">GitHub: 5</li>
+                </ul>
+              </div>
+              <div class="card employee-card">
+                  <div class="card-header">
+                    <h2 class="card-title"></h2>
+                    <h2 class="card-title">Fred</h2>
+                    <h3 class="card-title">Intern</h3>
+                  </div>
+                  <div class="card-body">
+                    <ul class="list-items">
+                      <li class="list-group-item">ID: 1</li>
+                      <li class="list-group-item">
+                        Email: <a href="mailto:bob@office.com">Intern@office.com</a>
+                      </li>
+                      <li class="list-group-item">University of Toronto</li>
+                    </ul>
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+    `;
 }
 
 runApplication();
--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
 
 test("Did Github get created?", () => {
