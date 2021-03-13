b546747affdab1bba645bf3ab922118663850662
Nathan McCaw <nathanmccaw@Analias-Mac-mini.local>
2021-03-13 16:14:06 -0500

    basic debugging

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const { create } = require("domain");
+// const { create } = require("domain");
 
 let employeeArray = [];
 let idArray = [];
@@ -14,7 +14,7 @@ function runInquirerManager() {
   inquirer.prompt = [
     {
       type: "input",
-      message: "Please enter your managers office number?",
+      message: "Please enter your managers name?",
       name: "managerName",
     },
     {
@@ -29,7 +29,7 @@ function runInquirerManager() {
     },
     {
       type: "input",
-      message: "Please enter your managers office number?",
+      message: "Please enter your managers phone number?",
       name: "officeID",
     },
   ].then((answers) => {
@@ -56,38 +56,16 @@ function runInquirerEmployee() {
   ].then((answers) => {
     switch (answers.title) {
       case "Engineer":
-        runInqurierEngineer();
+        runInquirerEngineer();
         break;
       case "Intern":
         runInquirerIntern();
         break;
       default:
-        createTeam();
+        createCard(employeeArray);
     }
   });
-
-  const inquirerPromps = [
-    {
-      type: "input",
-      message: "Please enter your name",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "Please enter your employee ID",
-      name: "id",
-    },
-    {
-      type: "list",
-      message: "Please select your title",
-      choices: ["Manager", "Engineer", "Intern"],
-      name: "title",
-    },
-  ];
-  return inquirer.prompt(inquirerPromps);
 }
-
-// Inquirer Prompts for Manager
 
 function runInquirerEngineer() {
   const promptArray = [
@@ -111,105 +89,105 @@ function runInquirerIntern() {
   return inquirer.prompt(promptArray);
 }
 
-async function runApplication() {
-  // Loop through employee array to create each object
-  let employeeArray = [];
-  for (i = 0; i < 1; i++) {
-    const promise = new Promise((resolve, reject) => {
-      runInquirerEmployee()
-        .then(function ({ name, id, email, title }) {
-          if (title === "Manager") {
-            runInquirerManager().then(function (officeID) {
-              this.employee = new Manager(name, id, email, officeID);
-              employeeArray.push(employee);
-              resolve("done");
-              console.log(employeeArray);
-            });
-          } else if (title === "Engineer") {
-            runInquirerEngineer().then(function ({ github }) {
-              this.employee = new Engineer(name, id, email, github);
-              employeeArray.push(employee);
-              resolve("done");
-            });
-          } else if (title === "Intern") {
-            runInquirerIntern().then(function ({ school }) {
-              this.employee = new Intern(name, id, email, school);
-              employeeArray.push(employee);
-              resolve("done");
-            });
-          }
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    });
-    const result = await promise;
-    console.log(result);
-    createCard(employeeArray);
-  }
+// async function runApplication() {
+//   // Loop through employee array to create each object
+//   let employeeArray = [];
+//   for (i = 0; i < 1; i++) {
+//     const promise = new Promise((resolve, reject) => {
+//       runInquirerEmployee()
+//         .then(function ({ name, id, email, title }) {
+//           if (title === "Manager") {
+//             runInquirerManager().then(function (officeID) {
+//               this.employee = new Manager(name, id, email, officeID);
+//               employeeArray.push(employee);
+//               resolve("done");
+//               console.log(employeeArray);
+//             });
+//           } else if (title === "Engineer") {
+//             runInquirerEngineer().then(function ({ github }) {
+//               this.employee = new Engineer(name, id, email, github);
+//               employeeArray.push(employee);
+//               resolve("done");
+//             });
+//           } else if (title === "Intern") {
+//             runInquirerIntern().then(function ({ school }) {
+//               this.employee = new Intern(name, id, email, school);
+//               employeeArray.push(employee);
+//               resolve("done");
+//             });
+//           }
+//         })
+//         .catch(function (err) {
+//           console.log(err);
+//         });
+//     });
+//     const result = await promise;
+//     console.log(result);
+//     createCard(employeeArray);
+//   }
 
-  let html = `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossorigin="anonymous"
-      />
-      <link rel="stylesheet" href="styles.css" />
-    </head>
-    <body>
-      <div class="container">
-        <div class="row">
-          <div class="team col-12 justify-content-center">
-            <div class="card employee-card">
-              <div class="card-header">
-              </div>
-              <div class="card-body">
-                <ul class="list-items">
-                  <li class="list-group-item">ID: 1</li>
-                  <li class="list-group-item">
-                  </li>
-                  <li class="list-group-item">Office Number: 5</li>
-                </ul>
-              </div>
-            </div>
-            <div class="card employee-card">
-              <div class="card-header">
-                <h2 class="card-title"></h2>
-              </div>
-              <div class="card-body">
-                <ul class="list-items">
-                  <li class="list-group-item">ID: 1</li>
-                  <li class="list-group-item">
-                  </li>
-                  <li class="list-group-item">GitHub: 5</li>
-                </ul>
-              </div>
-              <div class="card employee-card">
-                  <div class="card-header">
-                    <h2 class="card-title"></h2>
-                  </div>
-                  <div class="card-body">
-                    <ul class="list-items">
-                      <li class="list-group-item">ID: 1</li>
-                      <li class="list-group-item">
-                      </li>
-                    </ul>
-                  </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </body>
-  </html>
-    `;
-}
+//   let html = `<!DOCTYPE html>
+//   <html lang="en">
+//     <head>
+//       <meta charset="UTF-8" />
+//       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//       <title>Document</title>
+//       <link
+//         rel="stylesheet"
+//         href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+//         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+//         crossorigin="anonymous"
+//       />
+//       <link rel="stylesheet" href="styles.css" />
+//     </head>
+//     <body>
+//       <div class="container">
+//         <div class="row">
+//           <div class="team col-12 justify-content-center">
+//             <div class="card employee-card">
+//               <div class="card-header">
+//               </div>
+//               <div class="card-body">
+//                 <ul class="list-items">
+//                   <li class="list-group-item">ID: 1</li>
+//                   <li class="list-group-item">
+//                   </li>
+//                   <li class="list-group-item">Office Number: 5</li>
+//                 </ul>
+//               </div>
+//             </div>
+//             <div class="card employee-card">
+//               <div class="card-header">
+//                 <h2 class="card-title"></h2>
+//               </div>
+//               <div class="card-body">
+//                 <ul class="list-items">
+//                   <li class="list-group-item">ID: 1</li>
+//                   <li class="list-group-item">
+//                   </li>
+//                   <li class="list-group-item">GitHub: 5</li>
+//                 </ul>
+//               </div>
+//               <div class="card employee-card">
+//                   <div class="card-header">
+//                     <h2 class="card-title"></h2>
+//                   </div>
+//                   <div class="card-body">
+//                     <ul class="list-items">
+//                       <li class="list-group-item">ID: 1</li>
+//                       <li class="list-group-item">
+//                       </li>
+//                     </ul>
+//                   </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </body>
+//   </html>
+//     `;
+// }
 
 // Function to take inquirer input and generator content
 
@@ -281,4 +259,4 @@ function createCard(Employee) {
   });
 }
 
-runApplication();
+runInquirerManager();
